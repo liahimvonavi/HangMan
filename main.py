4c81a67d850c6a7ce161a4674c7edc876a2695e9
liahimvonavi <mihail.1994.ivanov@gmail.com>
2024-05-31 20:42:27 +0300

    final

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import random
 import asciiImage
-word = ["gypsy", "caramel", "star wars"]
-chosen_word = random.choice(word)
+import words
+print(f"{asciiImage.logo}\n")
+chosen_word = random.choice(words.word)
 shown_word = ["_"] *(len(chosen_word))
 same_input = []
 lives = 7
 while "_" in shown_word:
-    print(' '.join(shown_word))
+    print("The Word is: " +' '.join(shown_word))
     guessed = input("Guess a letter: \n").lower()
     if len(guessed)==1:
 
@@ -26,10 +27,10 @@ while "_" in shown_word:
 
                 print(asciiImage.stages[lives])
                 if lives == 0:
+                    print(f"The word was: {''.join(chosen_word)}")
                     print("You Lose!Game Over!")
                     exit()
     else:
         print("Invalid input!You can only choose one letter at a time!")
-fullword = ''.join(shown_word)
-print(fullword)
+print(f"The word is: {''.join(chosen_word)}\n")
 print("You Win ")
