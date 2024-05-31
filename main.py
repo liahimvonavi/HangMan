d4ba4190d62be18f71c3c83952839da6c9f238c6
liahimvonavi <mihail.1994.ivanov@gmail.com>
2024-05-31 19:47:02 +0300

    FIN

--- main.py
+++ main.py
@@ -2,22 +2,32 @@ import random
 word = ["gypsy", "caramel" "star wars"]
 chosen_word = random.choice(word)
 shown_word = ["_"] *(len(chosen_word))
+same_input = []
 lives = 6
 while "_" in shown_word:
     print(' '.join(shown_word))
     guessed = input("Guess a letter: \n").lower()
-    if guessed in chosen_word:
-        for index, letter in enumerate(chosen_word):
+    if len(guessed)==1:
 
-            if letter==guessed:
-                shown_word[index]=letter
+        if guessed in same_input:
+            print("You have already picked this letter try again with another one.")
 
+        else:
+            same_input.append(guessed)
+            if guessed in chosen_word:
+                for index, letter in enumerate(chosen_word):
+
+                    if letter==guessed:
+                        shown_word[index]=letter
+
+            else:
+                lives-=1
+                print(f"You have {lives} left\n")
+                if lives == 0:
+                    print("You Lose!Game Over!")
+                    exit()
     else:
-        lives-=1
-        print(f"You have {lives} left\n")
-        if lives == 0:
-            print("You Lose!Game Over!")
-            exit()
+        print("Invalid input!You can only choose one letter at a time!")
 fullword = ''.join(shown_word)
 print(fullword)
 print("You Win ")
